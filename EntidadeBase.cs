f607898856a5bf7be053a05e70d16d9eda743ebb
Talita Leal <86267204+lealtalita@users.noreply.github.com>
2022-03-09 22:22:51 -0300

    Add files via upload

---
+++ EntidadeBase.cs
@@ -0,0 +1,8 @@
+// See https://aka.ms/new-console-template for more information
+namespace App_cadastro_de_series.Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
